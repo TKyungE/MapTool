3f1d7e1d618aba3d4e93428c0b4402b0f0a6fe98
TKyungE <xow3953@naver.com>
2022-09-07 16:36:57 +0900

    1
    1

--- Tool/Private/MyTerrain.cpp
+++ Tool/Private/MyTerrain.cpp
@@ -97,7 +97,7 @@ HRESULT CMyTerrain::Render(void)
 	if (FAILED(m_pTransformCom->Bind_OnGraphicDev()))
 		return E_FAIL;
 
-	if (FAILED(m_pTextureCom->Bind_OnGraphicDev(1068)))
+	if (FAILED(m_pTextureCom->Bind_OnGraphicDev(393)))
 		return E_FAIL;
 
 	m_pVIBufferCom->Render();
